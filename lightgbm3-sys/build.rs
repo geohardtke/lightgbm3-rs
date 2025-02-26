75700fec40da6825b117fc82169d97ac624b0f65
geohardtke <48337686+geohardtke@users.noreply.github.com>
2025-02-27 09:16:16 +1000

    Update build.rs

--- lightgbm3-sys/build.rs
+++ lightgbm3-sys/build.rs
@@ -16,14 +16,11 @@ impl bindgen::callbacks::ParseCallbacks for DoxygenCallback {
 
 fn main() {
     let target = env::var("TARGET").unwrap();
-    let out_dir = env::var("BUILD_DIR").unwrap();
-    println!("out_dir (BUILD_DIR) {:?}\", out_dir);
+    let out_dir = env::var("OUT_DIR").unwrap();
     let lgbm_root = Path::new(&out_dir).join("lightgbm");
 
     // copy source code
-    let cmake_path = lgbm_root.join("CMakeLists.txt");
-    if !cmake_path.exists() {
-        println!("Debug -- not found, copy files!");
+    if !lgbm_root.exists() {
         let status = if target.contains("windows") {
             Command::new("cmd")
                 .args(&[
@@ -33,15 +30,10 @@ fn main() {
                 ])
                 .status()
         } else {
-            println!("Debug -- here, copy");
             Command::new("cp")
                 .args(&["-r", "lightgbm", lgbm_root.to_str().unwrap()])
-                .status().unwrap();
-            Command::new("chmod")
-                .args(&["-R", "755", lgbm_root.to_str().unwrap()])
                 .status()
         };
-
         if let Some(err) = status.err() {
             panic!(
                 "Failed to copy ./lightgbm to {}: {}",
@@ -52,18 +44,17 @@ fn main() {
     }
 
     // CMake
-    let mut cfg = Config::new(&lgbm_root)
+    let mut cfg = Config::new(&lgbm_root);
+    let cfg = cfg
         .profile("Release")
         .cxxflag("-std=c++11")
-        .define("BUILD_STATIC_LIB", "ON")
-        .define("USE_OPENMP", "OFF");
-
+        .define("BUILD_STATIC_LIB", "ON");
+    #[cfg(not(feature = "openmp"))]
+    let cfg = cfg.define("USE_OPENMP", "OFF");
     #[cfg(feature = "gpu")]
     let cfg = cfg.define("USE_GPU", "1");
-
     #[cfg(feature = "cuda")]
     let cfg = cfg.define("USE_CUDA", "1");
-
     let dst = cfg.build();
 
     // bindgen build
@@ -75,23 +66,20 @@ fn main() {
         .header("lightgbm/include/LightGBM/c_api.h")
         .allowlist_file("lightgbm/include/LightGBM/c_api.h")
         .clang_args(&clang_args)
-        .clang_arg(format!("{}{}", "-I", lgbm_root.join("include").display()))
+        .clang_arg(format!("-I{}", lgbm_root.join("include").display()))
         .parse_callbacks(Box::new(DoxygenCallback))
         .generate()
         .expect("Unable to generate bindings");
-
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .unwrap_or_else(|err| panic!("Couldn't write bindings: {err}"));
-
     // link to appropriate C++ lib
     if target.contains("apple") {
         println!("cargo:rustc-link-lib=c++");
     } else if target.contains("linux") {
         println!("cargo:rustc-link-lib=stdc++");
     }
-
     #[cfg(feature = "openmp")]
     {
         println!("cargo:rustc-link-args=-fopenmp");
@@ -109,10 +97,8 @@ fn main() {
             println!("cargo:rustc-link-lib=dylib=gomp");
         }
     }
-
     println!("cargo:rustc-link-search={}", out_path.join("lib").display());
     println!("cargo:rustc-link-search=native={}", dst.display());
-
     if target.contains("windows") {
         println!("cargo:rustc-link-lib=static=lib_lightgbm");
     } else {
